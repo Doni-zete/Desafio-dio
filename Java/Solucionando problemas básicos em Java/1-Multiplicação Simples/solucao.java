4749e0567baa06eb9dda07f43f00ef4b988fc0fd
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-03-25 00:41:00 -0300

    solucao

--- Java/Solucionando problemas básicos em Java/1-Multiplicação Simples/solucao.java
+++ Java/Solucionando problemas básicos em Java/1-Multiplicação Simples/solucao.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 
-public class MinhaPrimeiraClasse {
+public class solucao {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
