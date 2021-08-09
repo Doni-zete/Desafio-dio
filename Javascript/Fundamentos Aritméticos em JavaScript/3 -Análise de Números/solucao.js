ee7c71e7c25a0cfc10e52e71e2174c637fca1875
Doni-zete <donizete112@gmail.com>
2021-08-09 18:37:56 -0300

    ajuste

---
+++ Javascript/Fundamentos Aritméticos em JavaScript/3 -Análise de Números/solucao.js
@@ -0,0 +1,19 @@
+numero = Array(5);
+
+numero[0] = -5;
+numero[1] = 0;
+numero[2] = -3;
+numero[3] = -4;
+numero[4] = 12;
+
+pares = numero.filter((v) => Math.abs(v) % 2 ===0);
+impares = numero.filter((v) => Math.abs(v)  % 2 === 1);
+
+positivos = numero.filter((v) => v  > 0 );
+negativos = numero.filter((v) => v  <0 );
+
+console.log(pares.length + " valor(es) par(es)");
+console.log(impares.length + " valor(es) impar(es)");
+
+console.log(positivos.length + " valor(es) positivo(s)");
+console.log(negativos.length + " valor(es) negativo(s)");
