09ba94d6cdbb0fe9c28ded61ffffd46992acc1cf
Doni-zete <donizete112@gmail.com>
2021-08-11 23:54:11 -0300

    contagem de cedulas

--- Javascript/Fundamentos Aritméticos em JavaScript/4-Contagem de Cédulas/solucao.js
+++ Javascript/Fundamentos Aritméticos em JavaScript/4-Contagem de Cédulas/solucao.js
@@ -1,9 +1,19 @@
 let input = require("fs").readFileSync("stdin", "utf8");
 
 
-let valor = parseInt(input) ;
+let valor = parseInt(input);
 
 
 console.log(valor);
 
-const quantidadeNota = parseInt(valor/100);
+const quantidadeNotas = parseInt(valor / 100);
+
+console.log(quantidadeNotas);
+
+5 nota(s) de R$ 100,00
+1 nota(s) de R$ 50,00
+1 nota(s) de R$ 20,00
+0 nota(s) de R$ 10,00
+1 nota(s) de R$ 5,00
+0 nota(s) de R$ 2,00
+1 nota(s) de R$ 1,00
