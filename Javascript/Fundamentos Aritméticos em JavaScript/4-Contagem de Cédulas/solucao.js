e2a32a23c935a0fd4c3bfdf7a642e08a9f94c44e
Doni-zete <donizete112@gmail.com>
2021-08-12 00:30:47 -0300

    contagem de cedulas

--- Javascript/Fundamentos Aritméticos em JavaScript/4-Contagem de Cédulas/solucao.js
+++ Javascript/Fundamentos Aritméticos em JavaScript/4-Contagem de Cédulas/solucao.js
@@ -1,19 +1,35 @@
+/* Desafio
+Faça a leitura de um valor inteiro. 
+Em seguida, calcule o menor número de notas possíveis (cédulas) onde o valor pode ser decomposto. 
+As notas que você deve considerar são de 100, 50, 20, 10, 5, 2 e 1.
+ Na sequência mostre o valor lido e a relação de notas necessárias.
+
+Entrada
+Você receberá um valor inteiro N (0 < N < 1000000).
+
+Saída
+Exiba o valor lido e a quantidade mínima de notas de cada tipo necessárias, seguindo o exemplo de saída abaixo. Após cada linha deve ser imprimido o fim de linha. */
+
+
+
 let input = require("fs").readFileSync("stdin", "utf8");
 
 
 let valor = parseInt(input);
 
+let cedulas = [100, 50, 20, 10, 5, 2, 1]
 
 console.log(valor);
 
-const quantidadeNotas = parseInt(valor / 100);
 
-console.log(quantidadeNotas);
 
-5 nota(s) de R$ 100,00
-1 nota(s) de R$ 50,00
-1 nota(s) de R$ 20,00
-0 nota(s) de R$ 10,00
-1 nota(s) de R$ 5,00
-0 nota(s) de R$ 2,00
-1 nota(s) de R$ 1,00
+for (let cedula of cedulas) {
+
+    let quantidadeCedulas = parseInt(valor / cedula);
+    console.log(`${quantidadeCedulas} nota(s) de R$ ${cedula},00`);
+    valor = valor % cedula;
+}
+
+
+
+
