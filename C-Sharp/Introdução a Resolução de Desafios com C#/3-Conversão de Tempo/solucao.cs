7ecd79602e27e671aac2c773663537d7d1ce9fe6
Doni-zete <donizete112@gmail.com>
2021-09-11 12:31:26 -0300

    solucao

---
+++ C-Sharp/Introdução a Resolução de Desafios com C#/3-Conversão de Tempo/solucao.cs
@@ -0,0 +1,25 @@
+/* Desafio
+Você terá o desafio de ler um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma loja, e informe-o expresso no formato horas:minutos:segundos.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N.
+
+Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido. */
+
+using System;
+
+public class pizzaria
+{
+    public static void Main()
+    {
+        string[] line = Console.ReadLine().Split(" ");
+        int tempoSegundos = int.Parse(line[0]);
+
+        int horas = tempoSegundos / 3600;
+        int minutos = (tempoSegundos % 3600) / 60;
+        int segundos = (tempoSegundos % 3600) % 60;
+
+        Console.Write($"{horas}:{minutos}:{segundos}");
+    }
+}
