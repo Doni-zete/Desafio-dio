02b286680f760928d05109d469ee7d1e452276f7
Doni-zete <donizete112@gmail.com>
2021-09-15 00:00:15 -0300

    solucao

---
+++ C-Sharp/Solução de problemas em C#/3-Encaixa ou Não/solucao.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System.Linq;
+
+class {
+
+    static void Main(string[] args)
+    {
+        int qt = int.Parse(Console.ReadLine());
+        for (                 )
+        {
+            
+            string a = v[0];
+            string b = v[1];
+
+            if (                  )
+                Console.WriteLine("nao encaixa");
+            else if (                                )
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
+
+        }
+
+    }
+
+}
