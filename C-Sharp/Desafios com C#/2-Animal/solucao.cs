36f2ad780baa4caef3ed2a0b6a3d977d7f18d4e1
Doni-zete <donizete112@gmail.com>
2021-09-17 12:06:05 -0300

    solucao

---
+++ C-Sharp/Desafios com C#/2-Animal/solucao.cs
@@ -0,0 +1,49 @@
+/* Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas. */
+
+
+using System;
+using System.Text;
+
+class MainClass {
+  public static void Main (string[] args) {
+  String palavra1;
+  String palavra2;
+  String palavra3;
+  palavra1 = Console.ReadLine();
+  palavra2 = Console.ReadLine();
+  palavra3 = Console.ReadLine();
+
+
+    if (palavra1 == "vertebrado") {
+      if (palavra2 == "ave") {
+        if (palavra3 == "carnivoro") {
+          Console.WriteLine("aguia");
+        } else if (palavra3 == "onivoro") {
+          Console.WriteLine("pomba");
+        }
+      } else if (palavra2 == "mamifero") {
+        if (palavra3 == "onivoro") {
+          Console.WriteLine("homem");
+        } else if (palavra3 == "herbivoro") {
+          Console.WriteLine("vaca");
+        }
+      }
+    }
+
+    if (palavra1 == "invertebrado") {
+      if (palavra2 == "inseto") {
+        if (palavra3 == "hematofago") {
+          Console.WriteLine("pulga");
+        } else if (palavra3 == "herbivoro") {
+          Console.WriteLine("lagarta");
+        }
+      } else if (palavra2 == "anelideo") {
+        if (palavra3 == "hematofago") {
+          Console.WriteLine("sanguessuga");
+        } else if (palavra3 == "onivoro") {
+          Console.WriteLine("minhoca");
+        }
+      }
+    }
+  }
+}
