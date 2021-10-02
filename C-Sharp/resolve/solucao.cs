3a1db969f895e720186837907e7c8c5eae9d1094
Doni-zete <donizete112@gmail.com>
2021-10-01 23:12:25 -0300

    solucao

--- C-Sharp/resolve/solucao.cs
+++ C-Sharp/resolve/solucao.cs
@@ -1,52 +1,25 @@
-﻿/* Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+﻿/* O microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
 
 Entrada
-Leia três valores de ponto flutuante (double) A, B e C.
+A entrada é uma linha de texto T (1 ≤ |T| ≤ 500).
 
 Saída
-Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
- */
+A saída é dada em uma única linha. Ela deve ser "TWEET" (sem as aspas) se a linha de texto T tem até 140 caracteres. Se T tem mais de 140 caracteres, a saída deve ser "MUTE". */
+
 
 using System; 
 
-
-
 class minhaClasse {
 
-  static void Main(string[] args) { 
+    static void Main(string[] args) { 
 
-    double a, b, c, delta, r1, r2;
-
-    string[] valor = Console.ReadLine().Split();
-
-
-
-    a = Convert.ToDouble(valor[0]);
-
-    b = Convert.ToDouble(valor[1]);
-
-    c = Convert.ToDouble(valor[2]);
-
-    delta = (Math.Pow(b, 2) - (4 * a * c));
-
-    r1 = (-b + Math.Sqrt(delta)) / (2 * a);
-
-    r2 = (-b - Math.Sqrt(delta)) / (2 * a);
-
-
-
-    if (a != 0 && delta > 0) {
-
-      Console.WriteLine($"R1 = {r1.ToString("F5")}");
-
-      Console.WriteLine($"R2 = {r2.ToString("F5")}");
-
-    } else {
-
-      Console.WriteLine("Impossivel calcular");
+        string v = Console.ReadLine();
+            char[] arr = v.ToCharArray();
+            if (arr.Length <=140) //complete a condicional
+                Console.WriteLine("TWEET");
+            else
+                Console.WriteLine("MUTE");
 
     }
-
-  }
 
 }
