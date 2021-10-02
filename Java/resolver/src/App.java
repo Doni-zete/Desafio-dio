9c8e9c4f58a5ab748e7b13a8f030fbb6494ded5d
Doni-zete <donizete112@gmail.com>
2021-10-01 23:10:57 -0300

    soluao

---
+++ Java/resolver/src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int t = scan.nextInt();
+        
+        int count = 0; 
+        while(count < t) {
+        	String vilao = scan.next();
+        	System.out.println("Y"); //complete aqui com sua solução
+        	count++;
+        }       
+    }
+}
+
