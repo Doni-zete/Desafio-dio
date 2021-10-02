4859f12b86a4882d9337ecd91fff375e852f4d56
Doni-zete <donizete112@gmail.com>
2021-10-01 23:08:54 -0300

    solucao

---
+++ C-Sharp/Primeiros desafios em C#/2-Diferença/solucao.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int   //declare suas variaveis              ;
+           variavel = int.Parse(Console.ReadLine());
+          //continue a solução
+            Console.WriteLine("DIFERENCA = " +   (     *      -     *     ));
+            Console.ReadKey();
+        }
+    }
+}
