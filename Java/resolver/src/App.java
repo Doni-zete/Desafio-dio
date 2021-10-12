c39218b68227fab2442e82d0df35308721df8a18
Doni-zete <donizete112@gmail.com>
2021-10-12 00:42:50 -0300

    solucao

--- Java/resolver/src/App.java
+++ Java/resolver/src/App.java
@@ -1,17 +1,18 @@
+import java.io.IOException;
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        
-        int t = scan.nextInt();
-        
-        int count = 0; 
-        while(count < t) {
-        	String vilao = scan.next();
-        	System.out.println("Y"); //complete aqui com sua solução
-        	count++;
-        }       
+public class App{
+    
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        //continue a solucao
+        int  A  = leitor.nextInt();
+        int  B  = leitor.nextInt();
+        if (           ) {
+            System.out.println("Sao Multiplos");
+        } else {
+            
+        }
     }
+    
 }
-
