fd80b9af4acf8bf28f0fe88dfc8abdafb1a53897
Doni-zete <donizete112@gmail.com>
2021-10-26 21:27:37 -0300

    solucao

--- Java/Desafios numéricos em Java/3-Fatorial Simples/solucao.java
+++ Java/Desafios numéricos em Java/3-Fatorial Simples/solucao.java
@@ -1,11 +1,27 @@
+/*
+ 
+ QUESTÃO
+ Ler um valor N. Calcular e escrever seu respectivo fatorial. 
+ Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+ ENTRADA
+ A entrada contém um valor inteiro N (0 < N < 13).
+ SAÍDA
+ A saída contém um valor inteiro, correspondente ao fatorial de N.
+*/
+
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Problem {
-
-	public static void main(String[] args) {
+public class App {
 	
-              //complete o código com sua solução
-
-		
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int Num = leitor.nextInt();
+        int fatorial = 1;
+        for (int i = 1; i <= Num; i++) {
+        	fatorial *= i;
+        }
+        System.out.println(fatorial);
+    }
+	
 }
