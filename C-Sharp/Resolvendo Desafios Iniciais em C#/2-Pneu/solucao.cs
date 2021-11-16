66da9a3cd28a37f8f303bcc331adad610d52cf3a
Doni-zete <donizete112@gmail.com>
2021-11-16 17:26:15 -0300

    solucao

---
+++ C-Sharp/Resolvendo Desafios Iniciais em C#/2-Pneu/solucao.cs
@@ -0,0 +1,16 @@
+using System;
+
+class TESTE
+{
+  static void Main()
+  {
+    var A = int.Parse(Console.ReadLine());
+    var B = int.Parse(Console.ReadLine());
+
+    double diferenca = A - B;
+
+    Console.WriteLine(diferenca);
+    //Termine o código 
+
+  }
+}
