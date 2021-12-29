3ae905b04a81f1e669d3e2a891090b9816176f0a
Doni-zete <donizete112@gmail.com>
2021-12-28 21:39:35 -0300

    solucao

--- C-Sharp/Programando em C#/3-Sequência S/solucao.cs
+++ C-Sharp/Programando em C#/3-Sequência S/solucao.cs
@@ -1,18 +1,37 @@
-using System; 
+/* Escreva um algoritmo para calcular e escrever o 
+valor de S, sendo S dado pela fórmula:
+S = 1 + 1/2 + 1/3 + … + 1/100
 
-class DIO {
+Entrada
+Não há nenhuma entrada neste problema.
 
-    static void Main(string[] args) { 
+Saída
+A saída contém um valor correspondente ao valor de S.
+O valor deve ser impresso com dois dígitos após o ponto decimal.
 
-      double c, S= 0;
-            for ( int n =0; n<=100; n++     )
-            {
-                c =  1/n         ;  //coloque a sua lógica nos espaços em branco
-                S += c     ;
-            }
-            var x=Math.Round(S,2);
-            Console.WriteLine( s     );
+ 
+Exemplo de Entrada	Exemplo de Saída
+ 	 
+Agradecimentos a Cassio F.
+ */
 
+
+using System;
+
+class DIO
+{
+  static void Main(string[] args)
+  {
+
+    double c, S = 0;
+    for (double n = 1; n <= 100; n++)
+    {
+      c = 1 / n;  //coloque a sua lógica nos espaços em branco
+      S = S + c;
     }
+    var x = Math.Round(S, 2);
+    Console.WriteLine(x);
+
+  }
 
 }
