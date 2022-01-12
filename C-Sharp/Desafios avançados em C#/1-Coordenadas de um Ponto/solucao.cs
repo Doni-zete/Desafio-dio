6cc4db0760799e7c007ee13ec95c481a5652e6f4
Donizete Crisostomo <donizete112@gmail.com>
2022-01-11 22:59:52 -0300

    solucao

--- C-Sharp/Desafios avançados em C#/1-Coordenadas de um Ponto/solucao.cs
+++ C-Sharp/Desafios avançados em C#/1-Coordenadas de um Ponto/solucao.cs
@@ -1,4 +1,8 @@
-/* Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+/* Leia 2 valores com uma casa decimal (x e y), 
+que devem representar as coordenadas de um ponto em um plano.
+ A seguir, determine qual o quadrante ao qual pertence
+  o ponto, ou se está sobre um dos eixos
+   cartesianos ou na origem (x = y = 0).
 
 
 
