896738b576254a39afc5b2a888e2da2248082deb
Donizete Crisostomo <donizete112@gmail.com>
2022-01-31 20:51:56 -0300

    solucao

---
+++ Java/Desafios Iniciais - GFT Start #3 Java/2-Resto 2/solucao.java
@@ -0,0 +1,35 @@
+/* Desafio
+Leia um valor inteiro N. Apresente todos os números entre
+ 1 e 10000 que divididos por N dão resto igual a 2.
+
+Entrada
+A entrada contém um valor inteiro N (N < 10000).
+
+Saída
+Imprima todos valores que quando divididos por 
+N dão resto = 2, um por linha.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+13                             2
+                              15
+                              28
+                              41
+                              ... */
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class solucao {
+	
+	public static void main(String[] args) throws IOException {
+    	Scanner leitor = new Scanner(System.in);
+    	int N = leitor.nextInt();
+      int count;
+    	for (count=1;count<=10000;count++) {
+    		if (count%N==2     ) System.out.println(count      );
+    	}
+	}
+//Complete com a sua lógica nos espaços em branco	
+}
