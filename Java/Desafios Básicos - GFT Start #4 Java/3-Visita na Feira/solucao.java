54f4493406ab91ce5fcfbd34622d4afba7a365cd
Donizete Crisostomo <donizete112@gmail.com>
2022-02-20 22:28:00 -0300

    solucao

--- Java/Desafios Básicos - GFT Start #4 Java/3-Visita na Feira/solucao.java
+++ Java/Desafios Básicos - GFT Start #4 Java/3-Visita na Feira/solucao.java
@@ -13,7 +13,9 @@ Entrada
 A entrada contém 2 valores inteiros, separados por um espaço.
 
 Saída
-Imprimir a mensagem "X = " (sendo a letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Assegure que exista um espaço antes e depois do sinal de igualdade.
+Imprimir a mensagem "X = " (sendo a letra X maiúscula) 
+seguido pelo valor da variável X e pelo final de linha. 
+Assegure que exista um espaço antes e depois do sinal de igualdade.
 
  
 Exemplos de Entrada	Exemplos de Saída
