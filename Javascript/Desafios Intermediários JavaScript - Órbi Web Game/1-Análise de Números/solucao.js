a4e2f6afbe0a724f37819b838560a208bd089b62
Donizete Crisostomo <donizete112@gmail.com>
2022-04-21 21:31:09 -0300

    solucao

---
+++ Javascript/Desafios Intermediários JavaScript - Órbi Web Game/1-Análise de Números/solucao.js
@@ -0,0 +1,45 @@
+/* Você deve fazer a leitura de 5 valores inteiros.
+Em seguida mostre quantos valores informados são pares,
+ quantos valores informados são ímpares, quantos valores 
+ informados são positivos e quantos valores informados 
+ são negativos.Considere que o número zero é positivo, 
+ mas não pode ser considerado como positivo ou negativo.
+
+  Entrada
+Você receberá 5 valores inteiros.
+
+  Saída
+Exiba a mensagem conforme o exemplo de saída abaixo,
+ sendo uma mensagem por linha e não esquecendo o final de linha após cada uma.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+  - 5                 3 par(es)
+    0                 2 impar(es)
+  - 3                 1 positivo(s)
+  - 4                 3 negativo(s)
+    12
+ */
+let input = require("fs").readFileSync("stdin", "utf-8");
+let numero = input.split("\n");
+
+
+
+numero[0] = gets();
+numero[1] = gets();
+numero[2] = gets();
+numero[3] = gets();
+numero[4] = gets();
+
+
+console.log(numero[0]+numero[1]+numero[2]+numero[3]+numero[4]);
+
+/* pares = numero.filter(value => value % 2 == 0);
+impares = numero.filter(value => value % 2 != 0);
+positivos = numero.filter(value => value > 0);
+negativos = numero.filter(value => value < 0);
+
+console.log(pares.length + " par(es)");
+console.log(impares.length + " impar(es)");
+console.log(positivos.length + " positivo(s)");
+console.log(negativos.length + " negativo(s)"); */
